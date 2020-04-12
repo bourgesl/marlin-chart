47f430e1c1ce66a08b7957cc1a1e7fc2c79c19ed
bourgesl <bourges.laurent@gmail.com>
2020-04-12 17:02:53 +0200

    fixed env (zulu8 30 or 44)

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export OPENJDK8=/home/graphics-rasterizer/jdk/jdk8u-dev/build/linux-x86_64-normal-server-release/images/j2sdk-image/
+
 export ZULU8=/home/bourgesl/apps/zulu8.44.0.11-ca-jdk8.0.242-linux_x64/
+#export ZULU8=/home/bourgesl/apps/zulu8.30.0.1-jdk8.0.172-linux_x64/
 
